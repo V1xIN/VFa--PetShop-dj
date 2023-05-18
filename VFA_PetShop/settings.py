63a9f91b1ec082846f96a288da2952a6c1766e21
V1xIN <vi.verab@duocuc.cl>
2023-05-18 18:26:53 -0400

    Revert "clase 17-05"
    This reverts commit 74a952a92f12a9fe03e75f28357eae33c81216c8.

--- VFA_PetShop/settings.py
+++ VFA_PetShop/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
-from pathlib import Path, os
+from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'VFA_Pet',
 ]
 
 MIDDLEWARE = [
@@ -76,15 +75,8 @@ WSGI_APPLICATION = 'VFA_PetShop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.oracle',
-        'NAME': '127.0.0.1:1521/orcl',
-        'USER': 'prueba002D',
-        'PASSWORD': '12345678',
-        'TEST':{
-            'USER':'default_test',
-            'TBLSPACE':'default_test_tbls',
-            'TBLSPACE_TMP':'default_test_tbls_tmp',
-        },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
@@ -129,6 +121,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-MEDIA_URL= '/media/'
-MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
